b26f0e67a8425da5b1d231b5a1bc581137dcbef5
arnaudm <arnaud.mary@gmail.com>
2016-06-23 17:03:25 +0200

    Fix flash scope

--- src/connect/flash.js
+++ src/connect/flash.js
@@ -2,8 +2,10 @@
 
 module.exports = function(req, res, next) {
 
-  res.locals.flash  = req.session.flash || {};
+  req.session.flash = req.session.flash || {};
+  res.locals.flash  = req.session.flash;
   if (req.method === 'GET') {
+    // clear flash scope
     req.session.flash = {};
   }
   req.flash = function(key, value) {
