adc10f9a44f58c30ecd2df3a02e6a44775367b5a
arnaudm <arnaud.mary@gmail.com>
2016-12-06 10:58:09 +0100

    Fix dependency for production env

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ module.exports = {
   cls:      require('./src/cls'),
   config:   require('./src/config'),
   db:       require('./src/db/db'),
-  dev:      require('./src/dev/index'),
   i18next:  require('i18next'),
   render:   require('consolidate').dust,
   logger:   require('winston'),
   mailer:   require('./src/mailer'),
   Model:    require('./src/db/Model')
 };
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
+  module.exports.dev = require('./src/dev/index');
+}
