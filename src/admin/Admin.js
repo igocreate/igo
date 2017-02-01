874f8f294c8c80fd39eea02591c49bec4dbeba82
arnaudm <arnaud.mary@gmail.com>
2017-02-01 22:18:57 +0100

    Minor fix

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -18,7 +18,7 @@ const HtmlRenderer    = require('./HtmlRenderer');
 //
 const pluralize = function(name) {
   if (name.endsWith('y')) {
-    return name.substring(name.length - 1) + 'ies';
+    return name.substring(0, name.length - 1) + 'ies';
   }
   if (name.endsWith('x')) {
     return name + 'es';
