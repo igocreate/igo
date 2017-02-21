43c8c0a7e1045e0df03f68a0122d6fddea6ce4b4
arnaudm <arnaud.mary@gmail.com>
2017-02-21 15:49:00 +0100

    Allow async associations loading, release 2.3.19

--- src/db/Schema.js
+++ src/db/Schema.js
@@ -10,6 +10,14 @@ module.exports = class Schema {
     } else if (!_.isArray(schema.primary)) {
       schema.primary = schema.primary.split(',');
     }
+
+    // allow async associations loading for circular dependencies
+    process.nextTick(function() {
+      if (_.isFunction(schema.associations)) {
+        schema.associations = schema.associations();
+      };
+    });
+
     return schema;
   }
 }
