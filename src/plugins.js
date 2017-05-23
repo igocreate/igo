b3c11b9a932546aaa585dc0fdf4abaf82b9a57be
arnaudm <arnaud.mary@gmail.com>
2017-05-23 18:36:18 +0200

    Plugins : Remove double init check

--- src/plugins.js
+++ src/plugins.js
@@ -5,26 +5,17 @@
 const _       = require('lodash');
 const winston = require('winston');
 
-let init    = false;
-
-
 module.exports  = {
   list: []
 };
 
 module.exports.init = function() {
 
-  if (init) {
-    throw new Error('dont init plugins twice');
-    return;
-  }
-  init = true;
-
   // load app plugins
   try {
     const plugins = require(process.cwd() + '/app/plugins');
 
-    module.exports.list = _.values(plugins);
+    module.exports.list = plugins;
     module.exports.list.forEach(function(plugin) {
       // inject igo
       plugin.igo = require('../index');
