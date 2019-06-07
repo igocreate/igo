1ad30f8aa1eaff998cb5376bfe4d740f1db97641
Arnaud Mary <arnaud.mary@gmail.com>
2019-06-07 11:28:18 +0200

    Add 404 route

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,9 @@ const routes = require(process.cwd() + '/app/routes');
 //
 module.exports.init = function(app) {
   routes.init(app);
+
+  // 404
+  app.all('*', (req, res) => {
+    res.status(404).render('errors/404');
+  });
 };
