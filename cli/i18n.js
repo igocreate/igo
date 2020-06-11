19f10f443531e6d6bc27369e78d1449726c05945
Arnaud Mary <arnaud.mary@gmail.com>
2020-06-11 17:09:55 +0200

    I18n : export translations to csv

--- cli/i18n.js
+++ cli/i18n.js
@@ -2,7 +2,16 @@
 const request     = require('request');
 const fs          = require('fs');
 const _           = require('lodash');
+const async       = require('async');
+const csvWriter   = require('csv-write-stream');
+
 const config      = require('../src/config');
+
+const readJson = (path, callback) => {
+  fs.readFile(path, (err, data) => {
+    callback(err, JSON.parse(data)); 
+  });
+};
 
 // verbs
 const verbs   = {
@@ -57,6 +66,42 @@ const verbs   = {
 
       callback();
     });
+  },
+
+  // igo i18n csv
+  csv: function(args, callback) {
+    const langs = config.i18n.whitelist;
+    async.mapSeries(langs, (lang, callback) => {
+      readJson(`./locales/${lang}/translation.json`, callback);
+    }, (err, translations) => {
+      const keys    = [];
+      
+      // build keys
+      const traverse = (obj, parents) => {
+        for (const key in obj) {
+          const val = obj[key];
+          if (_.isObject(val)) {
+            traverse(obj[key], parents.concat([key]));
+          } else if (_.isString(val)) {
+            keys.push(parents.concat([key]).join('.'));
+          }
+        }
+      };
+      traverse(translations[0], []);
+
+      // build csv
+      const writer = csvWriter({ headers: [ 'key' ].concat(langs) });
+
+      writer.pipe(fs.createWriteStream('./translations.csv'))
+      keys.forEach(key => {
+        const row =  _.map(translations, translation => _.get(translation, key))
+        writer.write([ key ].concat(row))
+      });
+      
+      writer.end()
+      console.log(keys.length + ' lines written');
+      callback();
+    });
   }
 
 };
@@ -69,11 +114,11 @@ module.exports = function(argv) {
   if (args.length > 1 && verbs[args[1]]) {
     verbs[args[1]](args, function(err) {
       console.log(err || 'Done.');
-      process.exit(0);
+      // process.exit(0);
     });
   } else {
     console.error('ERROR: Wrong options');
-    console.error('Usage: igo i18n [update]')
+    console.error('Usage: igo i18n [update|csv]')
   }
 
 
