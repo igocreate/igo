b34e291fe65a446d0ea7a344e561e4157f00057b
Arnaud Mary <arnaud.mary@igocreate.com>
2022-04-13 14:44:08 +0200

    Replace mysql with mysql2, release 3.9.25

--- src/db/drivers/mysql.js
+++ src/db/drivers/mysql.js
@@ -1,11 +1,18 @@
 
-const mysql   = require('mysql');
+const _       = require('lodash');
+const mysql   = require('mysql2');
 
-
+const OPTIONS = [
+  'host', 'port', 'user', 'password', 'database',
+  'charset', 'debug', 'connectionLimit'
+];
+   
+   
 // create pool
 module.exports.createPool = (dbconfig) => {
-  return mysql.createPool(dbconfig);
+  return mysql.createPool(_.pick(dbconfig, OPTIONS));
 };
+
 
 // get connection
 module.exports.getConnection = (pool, callback) => {
