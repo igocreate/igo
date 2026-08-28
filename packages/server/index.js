91d7f512a7cdfff0d74592f8d690aa76d37ca6c3
Arnaud Mary <arnaud.mary@igocreate.com>
2026-08-28 14:58:47 +0200

    chore: clean exported objects from igo/server

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,25 +1,17 @@
 
-const db            = require('@igojs/db');
 const config        = require('./src/config');
 const cache         = require('./src/cache');
 const logger        = require('./src/logger');
-const utils         = require('./src/utils');
-const errorhandler  = require('./src/connect/errorhandler');
 
 const server = {
   app:        require('./src/app'),
   cache,
-  CacheStats: db.CacheStats,
   config,
   dev:        require('./src/dev/index'),
-  dbs:        db.dbs,
   express:    require('express'),
   i18next:    require('i18next'),
-  dust:       require('@igojs/dust'),
   logger,
   mailer:     require('./src/mailer'),
-  migrations: db.migrations,
-  Model:      db.Model,
   Form:       require('./src/forms/Form'),
 };
 
