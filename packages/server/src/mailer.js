8a09bba19a44f57b54b0c807f92b00e24ccc0cf2
Arnaud Mary <arnaud.mary@igocreate.com>
2026-09-16 10:50:18 +0200

    deps(server): upgrade mjml 4 → 5, nodemailer 9.1

--- packages/server/src/mailer.js
+++ packages/server/src/mailer.js
@@ -1,6 +1,6 @@
 const i18next     = require('i18next');
 const nodemailer  = require('nodemailer');
-const mjml2html   = require('mjml-4-terser');
+const mjml2html   = require('mjml');
 const fs          = require('fs');
 const path        = require('path');
 
